36af61fc9c9fa79028f385f5fca8c42082ee2010
magicreach134-cyber <magicreach134@gmail.com>
2025-10-16 17:55:39 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
+// next.config.js
+
+if (typeof window === "undefined") {
+  require("./shims/asyncStorage");
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // swcMinify: true, // Commented out to resolve Next.js warning
-  images: {
-    domains: ['localhost'],
-  },
 };
 
 module.exports = nextConfig;
