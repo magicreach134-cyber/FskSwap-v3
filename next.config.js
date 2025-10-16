e1b89f9fb0704cb9ef276442d96321faeea1fd38
magicreach134-cyber <magicreach134@gmail.com>
2025-10-16 13:38:48 +0100

    "Comment out swcMinify in next.config.js to fix warning".

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true, // Commented out to resolve Next.js warning
   images: {
     domains: ['localhost'],
   },
