42de1aa2ec568d8ea683d3b63ed8f70634ca82d1
magicreach134-cyber <magicreach134@gmail.com>
2025-12-05 19:18:33 +0100

    Create BTC.js

---
+++ Src/Utils/abis/BTC.js
@@ -0,0 +1,14 @@
+// utils/abis/BTC.js
+const BTC = [
+  // ... your BTC ABI here
+];
+export default BTC;
+
+// utils/abis/ETH.js
+const ETH = [
+  // ... your ETH ABI here
+];
+export default ETH;
+
+// Repeat for SOL.js, USDC.js, FUSDT.js, WBNB.js, Fressking.js, FSKFactory.js, FSKRouter.js,
+// FSKFeeCollector.js, FSKHelpFundAllPool.js, FSKLaunchpadFactory.js, FSKSwapLPStaking.js, IFSKRouter.js, FlashSwap.js
