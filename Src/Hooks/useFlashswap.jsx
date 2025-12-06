3bddc9735fb84780453770aeb44db909a3aa6875
magicreach134-cyber <magicreach134@gmail.com>
2025-12-06 11:36:05 +0100

    Create useFlashswap.jsx

---
+++ Src/Hooks/useFlashswap.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import { flashSwapABI, flashSwapAddress } from "../utils/constants";
+
+export const useFlashSwap = (signer) => {
+  const [flashSwapContract, setFlashSwapContract] = useState(null);
+
+  useState(() => {
+    if (signer) {
+      setFlashSwapContract(new ethers.Contract(flashSwapAddress, flashSwapABI, signer));
+    }
+  }, [signer]);
+
+  const estimateProfit = async (tokenBorrow, amount) => {
+    if (!flashSwapContract) return;
+    const profit = await flashSwapContract.estimateProfit(tokenBorrow, ethers.utils.parseUnits(amount.toString(), 18));
+    return ethers.utils.formatUnits(profit, 18);
+  };
+
+  const executeFlashSwap = async (tokenBorrow, amount) => {
+    if (!flashSwapContract) return;
+    const tx = await flashSwapContract.executeFlashSwap(tokenBorrow, ethers.utils.parseUnits(amount.toString(), 18));
+    return await tx.wait();
+  };
+
+  return { estimateProfit, executeFlashSwap };
+};
