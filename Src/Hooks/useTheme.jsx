6b6dd4eb8d820f8f9fa87f605f2c1c70b8824dda
magicreach134-cyber <magicreach134@gmail.com>
2025-12-07 12:49:01 +0100

    Create useTheme.jsx

---
+++ Src/Hooks/useTheme.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export const useTheme = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      setTheme(storedTheme);
+      document.documentElement.setAttribute("data-theme", storedTheme);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+  };
+
+  return { theme, toggleTheme };
+};
