f1cab888e6c6a1da8cf34ce0c4ad99d86bbe3068
magicreach134-cyber <magicreach134@gmail.com>
2025-12-07 11:10:41 +0100

    Update Router.jsx

--- Src/App/Router.jsx
+++ Src/App/Router.jsx
@@ -1,24 +1,27 @@
-"use client";
-
+// src/app/router.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// Pages
+import { ThemeProvider } from "../context/ThemeContext";
 import Swap from "../pages/Swap";
 import Launchpad from "../pages/Launchpad";
 import FlashSwap from "../pages/FlashSwap";
 import Staking from "../pages/Staking";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 const AppRouter = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Swap />} />
-        <Route path="/launchpad" element={<Launchpad />} />
-        <Route path="/flashswap" element={<FlashSwap />} />
-        <Route path="/staking" element={<Staking />} />
-      </Routes>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/swap" />} />
+          <Route path="/swap" element={<Swap />} />
+          <Route path="/launchpad" element={<Launchpad />} />
+          <Route path="/flashswap" element={<FlashSwap />} />
+          <Route path="/staking" element={<Staking />} />
+          {/* Add 404 fallback */}
+          <Route path="*" element={<div style={{ padding: "2rem" }}><h2>Page Not Found</h2></div>} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
