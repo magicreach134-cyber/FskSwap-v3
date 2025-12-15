17fe8e1efe3832b83c6425df10699888ed25ec8e
magicreach134-cyber <magicreach134@gmail.com>
2025-12-15 07:48:25 +0100

    Create web3-chains.tsx

---
+++ utils/lib/web3-chains.tsx
@@ -0,0 +1,27 @@
+import { Chain } from 'wagmi';
+
+export const bscTestnet: Chain = {
+  id: 97,
+  name: 'BNB Smart Chain Testnet',
+  network: 'bsc-testnet',
+  nativeCurrency: {
+    name: 'tBNB',
+    symbol: 'tBNB',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
+    },
+    public: {
+      http: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'BscScan',
+      url: 'https://testnet.bscscan.com',
+    },
+  },
+  testnet: true,
+};
