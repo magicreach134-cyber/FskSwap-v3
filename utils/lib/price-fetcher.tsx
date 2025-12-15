664c0ecc282b1e585bd51ae51c4ffbd5b8dd1096
magicreach134-cyber <magicreach134@gmail.com>
2025-12-15 11:31:59 +0100

    Update price-fetcher.tsx

--- utils/lib/price-fetcher.tsx
+++ utils/lib/price-fetcher.tsx
@@ -1,18 +1,12 @@
-import { BigNumber, ethers } from 'ethers';
-import { getRouterContract } from '@/lib/web3-client';
+import { BigNumber, ethers } from "ethers";
+import { getRouterContract } from "./web3-client";
 
 export const getAmountOut = async (
-  routerAddress: string,
   provider: ethers.providers.Provider,
   amountIn: BigNumber,
   path: string[]
 ): Promise<BigNumber> => {
-  const router = getRouterContract(routerAddress, provider);
-
-  const amounts: BigNumber[] = await router.getAmountsOut(
-    amountIn,
-    path
-  );
-
+  const router = getRouterContract(provider);
+  const amounts: BigNumber[] = await router.getAmountsOut(amountIn, path);
   return amounts[amounts.length - 1];
 };
