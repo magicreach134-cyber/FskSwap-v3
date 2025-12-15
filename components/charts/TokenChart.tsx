b4e66b322bbc5c7231f793f4a2903c7813b8dcc4
magicreach134-cyber <magicreach134@gmail.com>
2025-12-15 18:20:09 +0100

    Create TokenChart.tsx

---
+++ components/charts/TokenChart.tsx
@@ -0,0 +1,9 @@
+const TokenChart = () => {
+  return (
+    <div className="p-4 bg-white dark:bg-gray-800 rounded shadow">
+      <p className="text-gray-700 dark:text-gray-200">Token chart placeholder</p>
+    </div>
+  );
+};
+
+export default TokenChart;
