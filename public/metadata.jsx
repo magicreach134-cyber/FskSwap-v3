a35f9c064ad24e80e6fa3a8b66c670fa0ed0b32c
magicreach134-cyber <magicreach134@gmail.com>
2025-12-15 06:22:23 +0100

    Create metadata.jsx

---
+++ public/metadata.jsx
@@ -0,0 +1,15 @@
+{
+  "name": "FskSwap",
+  "short_name": "FskSwap",
+  "description": "The FSK DeFi multi-feature DEX for swap, farming, liquidity, launchpad, locker and staking.",
+  "icons": [
+    {
+      "src": "/metadata/site-icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ],
+  "theme_color": "#0b1120",
+  "background_color": "#0b1120",
+  "display": "standalone"
+}
