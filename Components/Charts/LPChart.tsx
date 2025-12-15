46610b0b2f77556e2dcc96854fe11c9c709332e3
magicreach134-cyber <magicreach134@gmail.com>
2025-12-15 18:21:21 +0100

    Delete Components directory

--- Components/Charts/LPChart.tsx
+++
@@ -1,9 +0,0 @@
-const LPChart = () => {
-  return (
-    <div className="p-4 bg-white dark:bg-gray-800 rounded shadow">
-      <p className="text-gray-700 dark:text-gray-200">LP chart placeholder</p>
-    </div>
-  );
-};
-
-export default LPChart;
