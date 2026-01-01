cd3e49426acba56fe355b6be4872bff0a8c7e26c
magicreach134-cyber <magicreach134@gmail.com>
2026-01-01 14:17:37 +0100

    Create web3Provider.tsx

---
+++ providers/web3Provider.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { WagmiProvider } from 'wagmi'
+import { RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { wagmiConfig } from '@/config/wagmi'
+
+const queryClient = new QueryClient()
+
+export function Web3Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          theme={darkTheme({
+            accentColor: '#F0B90B',
+            accentColorForeground: 'black',
+          })}
+        >
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
